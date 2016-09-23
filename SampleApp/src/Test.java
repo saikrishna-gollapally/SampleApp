39d9e120a5b1c3447852bb524f09f4b8b65dfcb8
SAIKRISHNA2 <saikrishna.gollapalli2@thrymr.net>
2016-09-23 11:54:55 +0530

    Added print message user2

--- SampleApp/src/Test.java
+++ SampleApp/src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("This is the first message..");
 		System.out.println("This is the second message..");
+		System.err.println("This is from user2");
 	}
 
 }
